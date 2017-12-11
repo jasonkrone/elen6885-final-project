8acd30e72b5604f81a5ae353597e17a22500e8be
Ubuntu <ubuntu@aws.instance>
2017-12-11 02:57:34 +0000

    Multi-head distillation. Using supervision from teachers and KL loss only

--- src/model.py
+++ src/model.py
@@ -186,3 +186,114 @@ class MLPPolicy(FFPolicy):
         x = F.tanh(x)
 
         return value, x
+
+
+class MultiHead_FFPolicy(nn.Module):
+    def __init__(self):
+        super(MultiHead_FFPolicy, self).__init__()
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def act(self, inputs, head, deterministic=False):
+        value, x = self(inputs)
+        #action = self.dist[head].sample(x, deterministic=deterministic)
+        if head == 0:
+            action = self.dist0.sample(x, deterministic=deterministic)
+        if head == 1:
+            action = self.dist1.sample(x, deterministic=deterministic)
+        return value, action
+
+    def evaluate_actions(self, inputs, actions, head):
+        value, x = self(inputs)
+        #action_log_probs, dist_entropy = self.dist[head].logprobs_and_entropy(x, actions)
+        if head == 0:
+            action_log_probs, dist_entropy = self.dist0.logprobs_and_entropy(x, actions)
+        if head == 1:
+            action_log_probs, dist_entropy = self.dist1.logprobs_and_entropy(x, actions)
+        return value, action_log_probs, dist_entropy
+
+    def get_mean_std(self, inputs, head):
+        value, x = self(inputs)
+        #action_mean, action_logstd = self.dist[head](x)
+        if head == 0:
+            action_mean, action_logstd = self.dist0(x)
+        if head == 1:
+            action_mean, action_logstd = self.dist1(x)
+        action_std = action_logstd.exp()
+        return action_mean, action_std
+
+
+
+class MultiHead_MLPPolicy(MultiHead_FFPolicy):
+    def __init__(self, num_inputs, action_space,num_heads):
+        """ Assumes all heads have same action space.
+        """
+        super(MultiHead_MLPPolicy, self).__init__()
+
+        self.obs_filter = ObsNorm((1, num_inputs), clip=5)
+        self.action_space = action_space
+        self.num_heads = num_heads
+
+        self.a_fc1 = nn.Linear(num_inputs, 64)
+        self.a_fc2 = nn.Linear(64, 64)
+
+        self.v_fc1 = nn.Linear(num_inputs, 64)
+        self.v_fc2 = nn.Linear(64, 64)
+        self.v_fc3 = nn.Linear(64, 1)
+
+        if action_space.__class__.__name__ == "Discrete":
+            num_outputs = action_space.n
+            self.dist = Categorical(64, num_outputs)
+        elif action_space.__class__.__name__ == "Box":
+            num_outputs = action_space.shape[0]
+            #self.dist = DiagGaussian(64, num_outputs)
+            assert self.num_heads <= 2
+            self.dist0 = DiagGaussian(64, num_outputs)
+            if self.num_heads == 2:
+                self.dist1 = DiagGaussian(64, num_outputs)
+        else:
+            raise NotImplementedError
+
+        self.train()
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.apply(weights_init_mlp)
+
+        """
+        tanh_gain = nn.init.calculate_gain('tanh')
+        self.a_fc1.weight.data.mul_(tanh_gain)
+        self.a_fc2.weight.data.mul_(tanh_gain)
+        self.v_fc1.weight.data.mul_(tanh_gain)
+        self.v_fc2.weight.data.mul_(tanh_gain)
+        """
+        #for head in range(self.num_heads):
+        #    if self.dist[head].__class__.__name__ == "DiagGaussian":
+        #        self.dist[head].fc_mean.weight.data.mul_(0.01)
+        if self.dist0.__class__.__name__ == "DiagGaussian":
+            self.dist0.fc_mean.weight.data.mul_(0.01)
+        if self.num_heads == 2:
+            if self.dist1.__class__.__name__ == "DiagGaussian":
+                self.dist1.fc_mean.weight.data.mul_(0.01)
+
+
+    def forward(self, inputs):
+        inputs.data = self.obs_filter(inputs.data)
+
+        x = self.v_fc1(inputs)
+        x = F.tanh(x)
+
+        x = self.v_fc2(x)
+        x = F.tanh(x)
+
+        x = self.v_fc3(x)
+        value = x
+
+        x = self.a_fc1(inputs)
+        x = F.tanh(x)
+
+        x = self.a_fc2(x)
+        x = F.tanh(x)
+
+        return value, x
